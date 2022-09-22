3b1e0e5092a31c9e0efc4afd5e6ab8d7d892017d
Switch <thunyoubun@gmail.com>
2022-09-22 16:37:50 +0700

    dd

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -7,16 +7,18 @@ export default function roomIdMessageIdRoute(req, res) {
     const roomId = req.query.roomId;
     const messageId = req.query.messageId;
 
-    const roomIdx = rooms.findIndex(
-      (x) =>
-        x.roomId === roomId &&
-        x.messages.findIndex((x) => x.messageId === messageId)
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+    const messageIdx = rooms[roomIdx].messages.findIndex(
+      (x) => x.messageId === messageId
     );
 
     if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+
+    if (messageIdx === -1)
       return res.status(404).json({ ok: false, message: "Invalid message id" });
 
-    rooms[roomIdx].splice(roomIdx, 1);
+    rooms[roomIdx].splice(messageIdx, 1);
 
     writeDB(rooms);
 
